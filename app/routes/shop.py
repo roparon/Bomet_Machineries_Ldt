54af1b4ae243d9649190d809f2000b2b65f5b318
Aron Rop <aaronrop40@gmail.com>
2025-10-02 21:29:58 +0300

    created templated folderand aded index and base.html

--- app/routes/shop.py
+++ app/routes/shop.py
@@ -84,9 +84,7 @@ def add_to_cart(product_id):
     cart.append(product)
     session["cart"] = cart
 
-    return redirect(url_for("shop.view_cart"))  # Redirect to the cart page
-
-# CART VIEW
+    return redirect(url_for("shop.view_cart")) 
 @shop.route("/cart")
 def view_cart():
     cart = session.get("cart", [])
