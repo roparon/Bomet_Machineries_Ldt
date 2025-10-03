de309161a48eb6ce024257023ddf1ff550c5c3ea
Aron Rop <aaronrop40@gmail.com>
2025-10-03 21:14:24 +0300

    created templated folderand aded index and base.html

--- app/routes/shop.py
+++ app/routes/shop.py
@@ -74,7 +74,6 @@ def add_to_cart(product_id):
     # Get the cart from session or create a new one
     cart = session.get("cart", [])
 
-    # Find the product
     product = next((p for p in products if p["id"] == product_id), None)
     if not product:
         return "Product not found", 404
