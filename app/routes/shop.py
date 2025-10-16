c57e886114ff221c1a17a8e7e0f459a5b93c97ee
Aron Rop <aaronrop40@gmail.com>
2025-10-16 19:54:48 +0300

    created templated folderand aded index and base.html

--- app/routes/shop.py
+++ app/routes/shop.py
@@ -71,7 +71,6 @@ def product_detail(product_id):
 
 @shop.route("/add_to_cart/<int:product_id>", methods=["POST"])
 def add_to_cart(product_id):
-    # Get the cart from session or create a new one
     cart = session.get("cart", [])
     product = next((p for p in products if p["id"] == product_id), None)
     if not product:
