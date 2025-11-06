cc474b45c8fc708379a584ffc87ef1b364873a59
Aron Rop <aaronrop40@gmail.com>
2025-11-06 20:36:06 +0300

    created templated folderand aded index and base.html

--- app/routes/shop.py
+++ app/routes/shop.py
@@ -21,7 +21,6 @@ def home():
             categorized_products[category] = []
         categorized_products[category].append(product)
 
-    # Pass categorized_products to template
     return render_template(
         "index.html",
         categorized_products=categorized_products,
