db6aac16fb990a1e6fc12e14a30ceeaeb60f67f2
Aron Rop <aaronrop40@gmail.com>
2025-11-07 22:23:09 +0300

    created templated folderand aded index and base.html

--- app/routes/shop.py
+++ app/routes/shop.py
@@ -25,7 +25,6 @@ def home():
         "index.html",
         categorized_products=categorized_products,
         current_year=datetime.now().year,
-        cart_count=len(session.get("cart", []))  # Optional cart support
     )
 
 
